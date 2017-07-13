9f5a8953d499fd4223182902c2e001bb4b0d630b
ApolloZhu <public-apollonian@outlook.com>
2017-07-12 22:17:19 -0400

    Manually add doc
    I’m kind of disappointed?

--- install-cmd.sh
+++ install-cmd.sh
@@ -4,3 +4,5 @@ cd Srt2BilibiliKit/Apps/terminal
 swift package clean
 swift build -c release
 cp .build/release/Srt2Bilibili-cli /usr/local/bin/s2bkit
+cd ../../..
+rm -rf Srt2BilibiliKit
---
+++ jazzy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo gem install jazzy
+jazzy --author ApolloZhu --author_url https://apollozhu.github.io --github_url https://github.com/ApolloZhu/Srt2BilibiliKit --module Srt2BilibiliKit
