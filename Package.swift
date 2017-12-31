069ae95054e07ae23594001f78768c17a56ea110
ApolloZhu <public-apollonian@outlook.com>
2017-12-31 00:19:34 -0500

    Fix jazzy and dependency

--- Package.swift
+++ Package.swift
@@ -17,10 +17,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        // .package(url: "https://github.com/ApolloZhu/BilibiliKit", .branch("master")),
-        // .package(url: "https://github.com/ApolloZhu/swift_qrcodejs", .branch("master")),
-        .package(url: "../BilibiliKit", .branch("master")),
-        .package(url: "../swift_qrcodejs", .branch("master")),
+        .package(url: "https://github.com/ApolloZhu/BilibiliKit", .branch("master")),
+        .package(url: "https://github.com/ApolloZhu/swift_qrcodejs", .branch("master")),
+        //.package(url: "../BilibiliKit", .branch("master")),
+        //.package(url: "../swift_qrcodejs", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
